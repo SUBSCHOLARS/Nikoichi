b3e810911c9291c2a2d35f0a3fd48a57d2eaf0a2
Kota Hatanaka <kenkotan7722@gmail.com>
2025-07-04 16:09:15 +0900

    引力発生機構コード書いた

---
+++ Nikoichi/Assets/Scripts/Gravity/GravityController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public float forceStreength = 100.0f;
+    private Rigidbody2D rb2D;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウスの左ボタンが長押しされている間だけ処理を行う
+        if (Input.GetMouseButton(0))
+        {
+            //1. マウスの座標をワールド座標に変換
+            Vector3 mouseScreenPos = Input.mousePosition;
+            //Z座標はカメラからの距離なので、適当な値を入れる
+            mouseScreenPos.z = 10f;
+            Vector2 targetPosition = mainCamera.ScreenToWorldPoint(mouseScreenPos);
+
+            //2. マウスの座標から対象のオブジェクトの位置を引く、位置によって強さが変わらないよう正規化する
+            Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
+            //3. 計算した方向に力を加える
+            rb2D.AddForce(direction * forceStreength * Time.deltaTime);
+        }
+    }
+}
